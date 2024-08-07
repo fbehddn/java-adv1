c67f6c3a9b9e2f94bcd329be477f8f7f53599236
fbehddn <tom0611@naver.com>
2024-08-07 15:59:55 +0900

    문제 4번 리팩토링

--- src/thread/start/test/StartTest4Main.java
+++ src/thread/start/test/StartTest4Main.java
@@ -4,33 +4,28 @@ import static util.MyLogger.log;
 
 public class StartTest4Main {
     public static void main(String[] args) {
-        Thread threadA = new Thread(new RunnableA(), "Thread-A");
-        Thread threadB = new Thread(new RunnableB(), "Thread-B");
+        Thread threadA = new Thread(new PrintWork("A", 1000),"Thread-A");
+        Thread threadB = new Thread(new PrintWork("B", 500), "Thread-B");
+
         threadA.start();
         threadB.start();
     }
 
-    static class RunnableA implements Runnable {
-        @Override
-        public void run() {
-            while (true) {
-                log("A");
-                try {
-                    Thread.sleep((1000));
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-    }
+    static class PrintWork implements Runnable {
+        private String content;
+        private int time;
 
-    static class RunnableB implements Runnable {
+        public PrintWork(String content, int time) {
+            this.content = content;
+            this.time = time;
+        }
+
         @Override
         public void run() {
             while (true) {
-                log("B");
+                log(content);
                 try {
-                    Thread.sleep((500));
+                    Thread.sleep(time);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
